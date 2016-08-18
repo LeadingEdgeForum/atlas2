007f6e775bbba73e85cfc7b12b6d2f2106e4f190
Krzysztof Daniel <krzysztof.daniel@gmail.com>
2016-08-18 14:59:44 +0200

    reworked the workspace list

--- atlas2/src-ui/pages/workspace/workspace-list-element.js
+++ atlas2/src-ui/pages/workspace/workspace-list-element.js
@@ -1,14 +1,35 @@
 /*jshint esversion: 6 */
 
 import React, {PropTypes} from 'react';
-import {ListGroupItem} from 'react-bootstrap';
+import {
+  Grid,
+  Row,
+  Col,
+  Jumbotron,
+  Button,
+  Table,
+  ListGroup,
+  ListGroupItem
+} from 'react-bootstrap';
 import LinkContainer from 'react-router-bootstrap';
+
 export default class WorkspaceListElement extends React.Component {
   render() {
-    var href = 'workspace/' + this.props.id;
+    var hrefOpen = 'workspace/' + this.props.id;
+    var hrefDeduplicate = 'deduplicate/' + this.props.id;
     return (
-      <ListGroupItem header={this.props.name} href={href}>
-        {this.props.description}
+      <ListGroupItem header={this.props.name}>
+        <Grid fluid={true}>
+          <Row className="show-grid">
+            <Col xs={9}>{this.props.description}</Col>
+            <Col xs={1}>
+              <Button bsStyle="default" href={hrefOpen}>Open</Button>
+            </Col>
+            <Col xs={2}>
+              <Button bsStyle="default" href={hrefDeduplicate}>Deduplicate</Button>
+            </Col>
+          </Row>
+        </Grid>
       </ListGroupItem>
     );
   }
