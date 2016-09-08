5d65a87b6ebfd305a16bf4281bdf8d569882d811
Chris Daniel <krzysztof.daniel@gmail.com>
2016-09-08 21:35:11 +0200

    small improvement to the dialog

--- atlas2/src-ui/pages/workspace/maps/editor/journey/transition.js
+++ atlas2/src-ui/pages/workspace/maps/editor/journey/transition.js
@@ -117,7 +117,7 @@ export default class Transition extends React.Component {
                   onMouseLeave={this.onMouseLeaveHandler.bind(this)}
                   onClick={this.onClick.bind(this)}>
             <Glyphicon glyph="chevron-right"/>
-            {text}
+            {text}{text ? <Glyphicon glyph="chevron-right"/> : null}
           </span>
       </span>);
   }
