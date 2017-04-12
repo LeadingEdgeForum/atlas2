deba49eeb094ed55eaa5207c70ba8f76e42dea1c
Chris Daniel <krzysztof.daniel@gmail.com>
2017-04-12 16:19:05 +0200

    do not put built js in the git
    auth0 needs to be configured at build time, so build before publish is
    mandatory

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-var */
+var path = require('path');
+var webpack = require('webpack');
+
+var config = require('./config.json');
+
+module.exports = {
+
+    // devtool: 'eval-source-map',
+
+    context: path.join(__dirname, 'src-ui'),
+
+    entry: {
+        app: './app',
+        'google-app': './google-app', // google
+        'l-p-app': './l-p-app', //login password, ldap & anonymous
+        'canvas-wrapper': './canvas-wrapper'
+    },
+
+    output: {
+        path: path.join(__dirname, 'build-ui/js'),
+        filename: '[name].js',
+        publicPath: '/js'
+    },
+
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel-loader'],
+            include: path.join(__dirname, 'src-ui')
+        }]
+    },
+
+    plugins: [new webpack.DefinePlugin({
+        ___AUTH0_AUDIENCE___: JSON.stringify(config.userProvider.auth0.audience),
+        ___AUTH0_ISSUER___: JSON.stringify(config.userProvider.auth0.issuer)
+    })]
+};
