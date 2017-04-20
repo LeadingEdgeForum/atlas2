dac5baba25125118d6026fb5b48cee9e97133092
Chris Daniel <krzysztof.daniel@gmail.com>
2017-04-20 10:29:56 +0200

    improved canvas wrapper

--- src-ui/canvas-wrapper.js
+++ src-ui/canvas-wrapper.js
@@ -1,23 +1,33 @@
+/* Copyright 2017 Krzysztof Daniel
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
 /*jshint esversion: 6 */
+/* globals document */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 var CWB = require('./pages/workspace/maps/editor/canvas-with-background.js').default;
 var jsPlumb = require("../node_modules/jsplumb/dist/js/jsplumb.min.js").jsPlumb;
 jsPlumb.init();
+
+/** map is passed via global variables, and once jsplumb is ready -
+we render a component. This is expected to be used server side. */
 jsPlumb.ready(function() {
-    ReactDOM.render( < CWB nodes = {
-            global.window.OPTS.nodes
-        }
-        comments = {
-            global.window.OPTS.comments
-        }
-        mapID = {
-            global.window.OPTS.mapID
-        }
-        workspaceID = {
-            global.window.OPTS.workspaceID
-        }
-        background = "true" / > ,
+    ReactDOM.render( <CWB
+          nodes       = {global.window.OPTS.nodes}
+          comments    = {global.window.OPTS.comments}
+          mapID       = {global.window.OPTS.mapID}
+          workspaceID = { global.window.OPTS.workspaceID}
+          background = "true" /> ,
         document.getElementById('root'));
 });
