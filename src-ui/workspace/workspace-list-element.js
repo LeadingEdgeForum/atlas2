e39406e2b77ab8d829242cff615794e16264222d
Chris Daniel <krzysztof.daniel@gmail.com>
2017-05-23 09:50:52 +0200

    Improve the workspace list

--- src-ui/workspace/workspace-list-element.js
+++ src-ui/workspace/workspace-list-element.js
@@ -1,24 +1,29 @@
 /*jshint esversion: 6 */
 
-import React, {PropTypes} from 'react';
+import React from 'react';
 import {
   Grid,
   Row,
   Col,
-  Jumbotron,
-  Button,
-  ButtonGroup,
   Table,
   ListGroup,
   ListGroupItem,
-  Glyphicon
+  Glyphicon,
+  DropdownButton,
+  MenuItem
 } from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
 import Actions from './workspace-actions';
 
 export default class WorkspaceListElement extends React.Component {
-  delete(id) {
+  delete(id, event) {
+    event.stopPropagation();
+    event.preventDefault();
     Actions.deleteWorkspace(id);
+  }
+  stopPropagation(event){
+    event.stopPropagation();
+    event.preventDefault();
   }
   render() {
     var workspaceID = this.props.id;
@@ -33,26 +38,22 @@ export default class WorkspaceListElement extends React.Component {
     } else {
       mapsCountInfo = "(" + mapsCount + " maps)";
     }
+    var dropDownTitle = <Glyphicon glyph="cog"></Glyphicon>;
     return (
-      <ListGroupItem header={this.props.name + " - " + this.props.purpose}>
-        <Grid fluid={true}>
-          <Row className="show-grid">
-            <Col xs={9}>{this.props.description} {mapsCountInfo}.</Col>
-            <Col xs={3}>
-              <ButtonGroup>
-                <LinkContainer to={{ pathname: hrefOpen }}>
-                  <Button bsStyle="default" href={hrefOpen}>
-                    <Glyphicon glyph="edit"></Glyphicon> Open
-                  </Button>
-                </LinkContainer>
-                <Button bsStyle="default" href="#" onClick={this.delete.bind(this, workspaceID)}>
-                  <Glyphicon glyph="remove"></Glyphicon> Remove
-                </Button>
-              </ButtonGroup>
-            </Col>
-          </Row>
-        </Grid>
-      </ListGroupItem>
+      <LinkContainer to={{ pathname: hrefOpen }}>
+        <ListGroupItem header={this.props.name + " - " + this.props.purpose} href={hrefOpen}>
+          <Grid fluid={true}>
+            <Row className="show-grid">
+              <Col xs={11}>{this.props.description} {mapsCountInfo}.</Col>
+              <Col xs={1}>
+                <DropdownButton title={dropDownTitle} onClick={this.stopPropagation}>
+                  <MenuItem eventKey="1" onClick={this.delete.bind(this, workspaceID)}><Glyphicon glyph="remove"></Glyphicon> Remove</MenuItem>
+                </DropdownButton>
+                </Col>
+              </Row>
+          </Grid>
+          </ListGroupItem>
+      </LinkContainer>
     );
   }
 }
