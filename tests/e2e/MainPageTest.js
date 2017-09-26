fe0b9ed53d1f24dcdd5122bba2e87e2f8a9ffe90
Chris Daniel <krzysztof.daniel@gmail.com>
2017-09-26 08:40:40 +0200

    another test improvement

--- tests/e2e/MainPageTest.js
+++ tests/e2e/MainPageTest.js
@@ -249,6 +249,7 @@ describe('Atlas 2 E2E tests', function() {
       browser.waitForVisible("a.list-group-item");
       while(browser.$('.list-group').$$('a.list-group-item').length > 0){
         browser.click('button.dropdown-toggle');
+        browser.waitForVisible(".glyphicon-remove");
         browser.click('.glyphicon-remove');
       }
   });
