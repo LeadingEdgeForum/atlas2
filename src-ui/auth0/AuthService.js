60271cf316c621e7fbfaeb1f2144e097212bb5a2
Chris Daniel <krzysztof.daniel@gmail.com>
2018-03-15 12:52:45 +0100

    Fix the auth0 dialog
    Conflicts:
    	src-ui/auth0/AuthService.js

--- src-ui/auth0/AuthService.js
+++ src-ui/auth0/AuthService.js
@@ -9,119 +9,123 @@ import Store from '../store';
 /* globals window */
 
 export default class AuthService extends Store {
-  constructor(clientId, domain) {
-    super();
 
-    /* globals ___AUTH0_AUDIENCE___ */
-    /* globals ___AUTH0_ISSUER___ */
-    let mustAcceptTerms = false;
-    let languageDictionary = {
-      title : 'Welcome to Atlas'/*,
-      signUpTerms : "I agree to <a href=\"/tos\" target=\"_new\">Terms of Service</a>."
-      */
-    };
-    this.auth0 = new Auth0Lock(___AUTH0_AUDIENCE___, ___AUTH0_ISSUER___, {
-      auth: {
-          responseType: 'token id_token',
-          redirect: true,
-          redirectUrl: window.location.origin,
-          params: {
-              scope: 'openid email user_metadata picture app_metadata'
-          }
-      },
-      theme : {
-        logo : '/img/LEF_logo.png',
-        primaryColor : '#00789b'
-      },
-      languageDictionary : languageDictionary,
-      mustAcceptTerms : mustAcceptTerms
-    });
+    constructor(clientId, domain) {
+        super();
 
-    this.login = this.login.bind(this);
-    this.signUp = this.signUp.bind(this);
-    this._doAuthentication = this._doAuthentication.bind(this);
-    this.setToken = this.setToken.bind(this);
-    this.getToken = this.getToken.bind(this);
-    this.loggedIn = this.loggedIn.bind(this);
-    this.logout = this.logout.bind(this);
-    let logger = function(arg){
-      return function(arg, msg) {
-          console.log('log', arg, msg);
-      }
+
+        let mustAcceptTerms = false;
+        let languageDictionary = {
+            title : 'Welcome to Atlas'
+            /*
+            ,
+            signUpTerms : "I agree to <a href=\"/tos\" target=\"_new\">Terms of Service</a>."
+            */
+        };
+        this.auth0 = new Auth0Lock(clientId, domain, {
+            auth: {
+                responseType: 'token id_token',
+                params: {
+                    scope: 'openid email user_metadata picture app_metadata'
+                }
+            },
+            theme : {
+                logo : '/img/LEF_logo.png',
+                primaryColor : '#00789b'
+            },
+            autoclose : true,
+            languageDictionary : languageDictionary,
+            mustAcceptTerms : mustAcceptTerms
+        });
+
+        this.login = this.login.bind(this);
+        this.signUp = this.signUp.bind(this);
+        this._doAuthentication = this._doAuthentication.bind(this);
+        this.setToken = this.setToken.bind(this);
+        this.getToken = this.getToken.bind(this);
+        this.loggedIn = this.loggedIn.bind(this);
+        this.logout = this.logout.bind(this);
+        this.auth0.on('authenticated', this._doAuthentication);
+        this.auth0.on('unrecoverable_error', console.error);
+        this.auth0.on('authorization_error', console.error);
     }
-    this.auth0.on('authenticated', this._doAuthentication);
-    this.auth0.on('authorization_error', logger('authorization_error'));
-      this.auth0.on('unrecoverable_error', logger('unrecoverable_error'));
-      this.auth0.on('hash_parsed', logger('hash_parsed'));
-  }
 
-  _doAuthentication(authResult) {
-    console.log('auth', authResult);
-      this.setToken(authResult.idToken);
-      this.emitChange();
-      if(this.history && this.location) {
-          this.history.push(this.location);
-      }
-      this.history = null;
-      this.location = null;
-  }
+    _doAuthentication(authResult) {
+        this.auth0.getUserInfo(authResult.accessToken, function(error, profile) {
+            if (error) {
+                // Handle error
+                console.log('error', error);
+                this.emitChange();
+                return;
+            }
 
-  next(location,history){
-    console.log(location, history);
-    this.location = location;
-    this.history = history;
-  }
+            localStorage.setItem("accessToken", authResult.accessToken);
+            localStorage.setItem("profile", JSON.stringify(profile));
 
-  setProfile(profile) {
-      // Saves profile data to local storage
-      localStorage.setItem('profile', JSON.stringify(profile));
-      // Triggers update
-      this.emitChange();
-  }
+            this.setToken(authResult.idToken);
+            this.setProfile(profile);
+            this.emitChange();
+            if(this.history && this.location) {
+                this.history.push(this.location);
+            }
+            this.history = null;
+            this.location = null;
+        }.bind(this));
+    }
 
-  getProfile() {
-      // Retrieves the profile data from local storage
-      const profile = localStorage.getItem('profile');
-      return profile ? JSON.parse(localStorage.profile) : {};
-  }
+    next(location,history){
+        this.location = location;
+        this.history = history;
+    }
 
-  login() {
-    this.auth0.removeAllListeners();
-    this.auth0.show({
-      allowSignUp : false,
-      allowLogin	 : true
-    });
-  }
+    setProfile(profile) {
+        // Saves profile data to local storage
+        localStorage.setItem('profile', JSON.stringify(profile));
+    }
 
-  signUp(){
-    this.auth0.show({
-      allowSignUp : true,
-      allowLogin	 : false
-    });
-  }
+    getProfile() {
+        // Retrieves the profile data from local storage
+        const profile = localStorage.getItem('profile');
+        return profile ? JSON.parse(localStorage.profile) : {};
+    }
 
-  loggedIn() {
-    const token = this.getToken();
-    const loggedIn = !!token && !isTokenExpired(token);
-    console.log('loggedIn', loggedIn, token);
-    return loggedIn;
-  }
+    login() {
+        this.auth0.show({
+            allowSignUp : false,
+            allowLogin	 : true
+        });
+    }
 
-  setToken(idToken) {
-    console.log('set token', idToken);
-    localStorage.setItem('id_token', idToken);
-  }
+    signUp(){
+        this.auth0.show({
+            allowSignUp : true,
+            allowLogin	 : false
+        });
+    }
 
-  getToken() {
-    // Retrieves the user token from local storage
-    return localStorage.getItem('id_token');
-  }
+    loggedIn() {
+        const token = this.getToken();
+        return !!token && !isTokenExpired(token);
+    }
 
-  logout() {
-      // console.log('logout', idToken);
-    // Clear user token and profile data from local storage
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('profile');
-    this.emitChange();
-  }
+    setToken(idToken) {
+        localStorage.setItem('id_token', idToken);
+    }
+
+    getToken() {
+        // Retrieves the user token from local storage
+        return localStorage.getItem('id_token');
+    }
+
+    getAccessToken(){
+        return localStorage.getItem('accessToken');
+    }
+
+    logout() {
+        // Clear user token and profile data from local storage
+        localStorage.removeItem('id_token');
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem('profile');
+        this.emitChange();
+    }
 }
