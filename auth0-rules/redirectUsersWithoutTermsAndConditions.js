1b553ff8ef359362a397ecdc187751b208b3377f
Chris <krzysztof.daniel@gmail.com>
2018-03-21 16:02:03 +0100

    update the auth0 rule

--- auth0-rules/redirectUsersWithoutTermsAndConditions.js
+++ auth0-rules/redirectUsersWithoutTermsAndConditions.js
@@ -1,5 +1,9 @@
-// This is a rule for auth0
+// verify tos - this is an AUTH0 rule that verifies that the user has accepted terms and conditions.
+// it works for one clientID - do not forget to set it.
 function (user, context, callback) {
+  if(context.clientID !== "Client ID - set me or I will not work") {
+    return callback(null, user, context);
+  }
   var termsAccepted = user && user.app_metadata && user.app_metadata.termsAccepted;
   if (context.protocol === "redirect-callback") {
     user.user_metadata = user.user_metadata || {};
@@ -8,12 +12,12 @@ function (user, context, callback) {
       version: 1 // one day retrieve it from the tool
     };
     auth0.users.updateAppMetadata(user.user_id, user.user_metadata)
-      .then(function(){
-          callback(null, user, context);
-      })
-      .catch(function(err){
-          callback(err);
-      });
+    .then(function(){
+        callback(null, user, context);
+    })
+    .catch(function(err){
+        callback(err);
+    });
   } else if(!termsAccepted){
     context.redirect = {
       url: "https://atlas2.wardleymaps.com/accept-tos"
